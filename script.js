60eef7017a25d5b99332b49e8046d723e0290fd5
luimoco <es.luimoco@gmail.com>
2024-11-06 22:10:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,28 @@ function generateExerciseEscribir(question) {
     const questionElement = document.querySelector(".question");
     questionElement.textContent = question;
 }
+
 function comprobar() {
     let respuestaUsuario = document.getElementById("inputRespuestaUsuario").value.toLowerCase();
     // Remove punctuation characters
     respuestaUsuario = respuestaUsuario.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-
-    if (respuestaUsuario === respuesta) {
-        document.querySelector(".feedback").innerHTML = "<p>ZORIONAK! " + pregunta + " " + respuestaUsuario + " da!</p><img src='./ondo.png' width = '64', height = '64' alt='Ondo'>";
+    // Trim leading and trailing spaces
+    respuestaUsuario = respuestaUsuario.trim();
+    const correctAnswers = respuesta.split("|");
+    let isCorrect = false;
+    for (const correctAnswer of correctAnswers) {
+        if (respuestaUsuario === correctAnswer.trim()) {
+            isCorrect = true;
+            break;
+        }
+    }
+    if (isCorrect) {
+        document.querySelector(".feedback").innerHTML = "<p>ZORIONAK! " + pregunta + " <span class='correcto'>" + respuesta.replace(/\|/g, "</span> edo <span class='correcto'>") + "</span> da!" + "</p><img src='./ondo.png' width = '64', height = '64' alt='Ondo'>";
     } else {
-        document.querySelector(".feedback").innerHTML = "<p><span class='incorrecto'>" + respuestaUsuario + "</span> ez da " + pregunta + ". " + pregunta + ", <b>" + respuesta + "</b> da.</p><img src='./txarra.png' width = '64', height = '64' alt='Txarra'>";
+        document.querySelector(".feedback").innerHTML = "<p><span class='incorrecto'>" + respuestaUsuario + "</span> ez da " + pregunta + ". " + pregunta + ", <span class='correcto'><b>" + respuesta.replace(/\|/g, "</span></b> edo <b><span class='correcto'>") + "</b></span> da.</p><img src='./txarra.png' width = '64', height = '64' alt='Txarra'>";
     }
 }
+
 // Funcionalidad para poder usar enter en los ejercicios de tipo Escritura
 const inputField = document.getElementById("inputRespuestaUsuario");
 if(inputField){
@@ -45,7 +56,7 @@ function comprobarOrden() {
         for (let i = 0; i < orderedWords.length; i++) {
             respuestaUsuario += orderedWords[i].textContent + " ";
         }
-        document.querySelector(".feedback").innerHTML = "<p>Ez da <span class='incorrecto'>" + respuestaUsuario.slice(0,-1) + "</span>. Errepikatu, mesedez.</p><img src='./txarra.png' width = '64', height = '64' alt='Txarra'>";
+        document.querySelector(".feedback").innerHTML = "<p>Ez da <span class='incorrecto'>" + respuestaUsuario.slice(0,-1) + "</span>.</p><p>" + pregunta + ". " + respuesta + " da!</p><img src='./txarra.png' width = '64', height = '64' alt='Txarra'>";
     }
 }
 
@@ -95,4 +106,30 @@ if (tipo == "ordenar") {
     generateExerciseOrdenar(pregunta, respuesta);
 } else if (tipo == "escribir") {
     generateExerciseEscribir(pregunta);
+}
+
+
+function loadRandomExercise() {
+    const MIN_EXERCISE_NUMBER = 1;
+    const MAX_EXERCISE_NUMBER = 4;
+    const randomNum = Math.floor(Math.random() * (MAX_EXERCISE_NUMBER - MIN_EXERCISE_NUMBER + 1)) + MIN_EXERCISE_NUMBER;
+    window.location.href = `preg${randomNum}.html`;
+}
+
+let buttonClicked = false; // Variable to track if the button has been clicked
+
+function checkAndChangeButton() {
+    if (!buttonClicked) {
+        if (tipo == "ordenar") {
+            comprobarOrden();
+        } else if (tipo == "escribir") {
+            comprobar();
+        }        
+        buttonClicked = true;
+        const checkButton = document.getElementById("checkButton");
+        checkButton.textContent = "Urrengo aldera";
+        checkButton.onclick = loadRandomExercise;
+    } else {
+        loadRandomExercise();
+    }
 }
